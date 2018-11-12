c41d22eef18954d4206e14817fc232ce47afb0aa
v1km4n <34266885+v1km4n@users.noreply.github.com>
2018-11-13 00:50:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,18 @@ client.on("message", (message) => {
 	if(message.content == "!webmub_kogda_vidos"){ 
 	message.reply("<:webmube_king:378905849864912896>");
 	} 
-	if(message.content == "!citata"){ //начало второго условия
+	if(message.content == "!citata"){
 	answer = ["витя красавчик: превед англечане", "sobaque: ЗАТЫКАЛ - kill В КОНСОЛЬ", 
 	"Zush: Обожаю сидеть и пердеть", "Kworker: ну это палка на двух концах", "summer: йообаный чернослив, пацаны", "Ritz: А мы тут дегротом балуемся :smirk:",
 	 "vika: хотел как анимешная девочка чихнуть, а получилась какая-то хуйня",]; //Массив с возможными ответами
 	message.channel.send(answer[Math.floor(Math.random() * answer.length)]);
+	}
+	if(message.content == "!derzhi"){
+	let role = message.guild.roles.find(r => r.name === "Держите в курсе");
+	
+	let member = message.mentions.members.first();
+	
+	member.addRole(role).catch(console.error);
 	}
 });
 client.on("ready", ()=>{
