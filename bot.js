3b116116bbf25af231a1bceeb146fa4b9e729cf9
v1km4n <34266885+v1km4n@users.noreply.github.com>
2019-02-22 02:56:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,10 +19,8 @@ client.on("message", (message) => {
 	message.channel.send(answer[Math.floor(Math.random() * answer.length)]);
 	}
 	if(message.content == "!wise"){
-
-		const attachment = new MessageAttachment('ну и в пизду, я хуй его знает, как эти аттачменты ебаные делать');
-		//http://puu.sh/C0KWv/752fdc8f1a.PNG
-		message.channel.send(attachment);
+		const attachment = new Attachment('http://puu.sh/C0KWv/752fdc8f1a.PNG');
+       		message.channel.send(attachment);
 		
 	}
 	if(message.content == "!non"){
