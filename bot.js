9364ef959e1ae2944cf1e09cb41a1d870c8e0c07
v1km4n <34266885+v1km4n@users.noreply.github.com>
2019-07-01 20:59:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,9 +71,9 @@ client.on("message", (message) => {
     	}
 	if (message.content == '!chaninfo') {
 	chan = message.channel;
-		client.channels.get("511298295985864714").send(chan.toString());
-	message.channel.send(chan.createdAt.toString());
-    	}
+	message.channel.send(chan.createdAt.date + " " + (chan.createdAt.month + 1) + " " + chan.createdAt.year);
+		
+	
 });
 
 client.on("ready", ()=>{
