5ece505cdf758a305f2cc45f79da715cb228298b
v1km4n <34266885+v1km4n@users.noreply.github.com>
2020-03-08 19:03:53 +0300

    initial commit (#5)

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = '!';
 const SteamAPI = require('steamapi');
-
+const notificationsRoleID = '511657769233809408';
 const steam = new SteamAPI(process.env.STEAM_TOKEN);
 client.login(process.env.BOT_TOKEN);
 
@@ -66,16 +66,24 @@ client.on('message', message => {
 		message.channel.send(answer[Math.floor(Math.random() * answer.length)]);
 	}
 
-	if (command === 'non')
+	if (command === 'non' && !member.roles.has(notificationsRoleID))
 	{
-		message.member.roles.add("511657769233809408");
+		message.member.roles.add(notificationsRoleID);
 		message.reply("теперь ты занесён в список поддерживаемых в курсе. Чтобы отписаться напиши команду !noff");
+	} 
+		else if (member.roles.has(notificationsRoleID))
+	{
+		message.reply("у тебя уже есть роль")
 	}
 
-	if (command === 'noff')
+	if (command === 'noff' && member.roles.has(notificationsRoleID))
 	{
 		message.member.roles.remove("511657769233809408");
 		message.reply("ты удалён из списка поддерживаемых в курсе. Чтобы подписаться обратно напиши команду !non");
+	} 
+		else if (!member.roles.has(notificationsRoleID))
+	{
+		message.reply("у тебя и нет никакой роли")
 	}
 
 	if (command === 'catgirl')
