cabae7e0b00644a8fa824e6967cb5d4983f7288f
v1km4n <v1km4n@ya.ru>
2020-03-08 21:38:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -164,8 +164,8 @@ client.on('message', async message => {
 	}
 
 	if (command === 'volume') {
-		player_volume = args[0]*10;
-		message.channel.send('Volume is now: ${player_volume}')
+		player_volume = args[0] / 10;
+		message.channel.send('Volume is now: ${args[0]}%');
 	}
 
 	if (command === 'play') {
