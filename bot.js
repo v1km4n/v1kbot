fa0a655a6153abc36fd470562e77fde9eedfd0ca
v1km4n <v1km4n@ya.ru>
2020-03-10 21:36:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -188,7 +188,7 @@ client.on('message', async message => {
 
 		let user_calling = message.member;
 		const connection = await user_calling.voice.channel.join(); 
-		const dispatcher = connection.play(ytdl(player_queue[old_amount + 1], { quality: 'highestaudio' }));
+		const dispatcher = connection.play(ytdl(player_queue[old_amount], { quality: 'highestaudio' }));
 
 		function play(url) {
 			connection.play(ytdl(url, { quality: 'highestaudio' }));
