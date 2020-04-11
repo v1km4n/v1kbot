32ec9a03b3f2025cd0daafd39f9cc23e2dcf5600
v1km4n <v1km4n@ya.ru>
2020-04-11 03:30:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -213,7 +213,6 @@ client.on('message', async message => {
 
 	async function url_handler(url, client, connection, queue) {
 		var info = await ytdl.getInfo(url);
-		console.log('got this ' + info.title);
 		var guildID = message.guild.id;
 
 		queue.push({
@@ -225,7 +224,7 @@ client.on('message', async message => {
 
 		let user_calling = message.member;
 		if (!connection) connection = await user_calling.voice.channel.join(); 
-		if (!dispatcher) await play(client, connection, queue, guildID)
+		if (!dispatcher) play(client, connection, queue, guildID)
 		else {
 			message.channel.send(`Added \`${info.title}\` to the Queue | Requested by \`${message.author.tag}\``);
 		}
@@ -234,6 +233,7 @@ client.on('message', async message => {
 	async function play(client, connection, queue, guildID) {
 		console.log(queue);
 		client.channels.cache.get(queue[0].channel).send(`Now playing \`${queue[0].songName}\` | Requested by \`${queue[0].requester}\``);
+		console.log(connection + ' ' + connection.play);
 		dispatcher = await connection.play(ytdl(queue[0].url, { filter: 'audioonly' }));
 		dispatcher.guildID = guildID;
 
@@ -242,13 +242,13 @@ client.on('message', async message => {
 		})
 	}
 
-	async function finish(client, queue, guildID, shift_amount) {
+	function finish(client, queue, guildID, shift_amount) {
 		for (var i = 0; i < shift_amount; ++i){
 			queue.shift();
 		}
 
 		if (queue.length > 0) {
-			await play(client, connection, queue, guildID);
+			play(client, connection, queue, guildID);
 		} else {
 			let voice_channel = client.guilds.cache.get(guildID).me.voice.channel;
 			if (voice_channel) voice_channel.leave();
