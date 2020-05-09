0f80193eceebc40a8db651ec2d96be1fc3bcac17
v1km4n <34266885+v1km4n@users.noreply.github.com>
2020-05-10 01:59:00 +0300

    Dmb (#130)
    * Update bot.js
    
    * Update bot.js
    
    * Update bot.js

--- bot.js
+++ bot.js
@@ -138,9 +138,21 @@ client.on('message', async message => {
 	}
 
 	if (command === 'dmb') {
-		var currentDate = Date.now();
-		var difference = stars - currentDate;
-		message.channel.send(`${difference} ms`);
+		let difference = stars - Date.now();
+		let answer = ms_to_date(difference);
+		message.channel.send(`${answer}`);
+	}
+
+	function ms_to_date (ms) {
+		var mss = ms % 1000;
+		ms = (ms - mss) / 1000; //secs
+		var secs = ms % 60;
+		ms = (ms - secs) / 60; //mins
+		var mins = ms % 60;
+		ms = (ms - mins) / 60; //hrs
+		var hrs = ms % 24;
+		ms = (ms - hrs) / 24; // days
+		return `${ms} days, ${hrs} hrs, ${mins} mins, ${secs} secs`;
 	}
 
 	if (command === 'match') {
