cf2104728f9477d494a9c697c3b2f81050259b63
v1km4n <v1km4n@ya.ru>
2020-09-16 16:09:44 +0300

    Update main.js

--- main.js
+++ main.js
@@ -83,7 +83,17 @@ client.on('message', async message => {
 		var request = new XMLHttpRequest();
 		var UGCLink;
 		var RGLLink;
-    	var ETF2LLink;
+		var ETF2LLink;
+		
+		var embedWithLeaguesLinks = new Discord.MessageEmbed()
+				.setColor('#0099ff')
+				.setTitle(`League Links for **${NickName}**`)
+				.setThumbnail(ProfilePicture)
+				.addFields(
+					{ name: '**ETF2L**', value: ETF2LLink},
+					{ name: '**UGC**', value: UGCLink},
+					{ name: '**RGL**', value: RGLLink},
+				)
 
 		steam.resolve(args[0]).then(steamID => {			
 			// UGC
@@ -109,6 +119,7 @@ client.on('message', async message => {
 				NickName = etf2lPlayer.player.name;
 				ProfilePicture = etf2lPlayer.player.steam.avatar;
 				ETF2LLink = "https://etf2l.org/forum/user/" + ETF2LID;
+				message.channel.send(embedWithLeaguesLinks);
 			} else { //if not, take the nickname and the profile picture from the steam profile
 				console.log("got not 200");
 				steam.getUserSummary(steamID).then(summary => {
@@ -116,22 +127,11 @@ client.on('message', async message => {
 					console.log(`got steam nickname: ${NickName}`);
 					ProfilePicture = summary.avatar.large;
 					console.log(`got steam pfp link: ${ProfilePicture}`);
+					ETF2LLink = "none";
+					message.channel.send(embedWithLeaguesLinks);
 				});
-				console.log(`got steam pfp link: ${ProfilePicture}`);
-				ETF2LLink = "none";
+				console.log(`got steam pfp link: ${ProfilePicture}`);		
 			}
-
-			var embedWithLeaguesLinks = new Discord.MessageEmbed()
-				.setColor('#0099ff')
-				.setTitle(`League Links for **${NickName}**`)
-				.setThumbnail(ProfilePicture)
-				.addFields(
-					{ name: '**ETF2L**', value: ETF2LLink},
-					{ name: '**UGC**', value: UGCLink},
-					{ name: '**RGL**', value: RGLLink},
-				)
-			message.channel.send(embedWithLeaguesLinks);
-
 		});
 	}
 	
