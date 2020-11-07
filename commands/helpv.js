a0a5269207bd91f64d9937dddbb05cc4fe16e46d
v1km4n <v1km4n@ya.ru>
2020-11-08 01:52:46 +0300

    Update helpv.js

--- commands/helpv.js
+++ commands/helpv.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'helpv',
 	description: 'Main Bot Commands',
 	execute(message, args) {
-		message.author.send(`\`!non/!noff - уведомления на сервере
+		message.author.send(`\`\`\`!non/!noff - уведомления на сервере
 !catgirl - :(
 !chaninfo - дата создания канала (хз зачем я это сделал)
 !cbt - кокенболторчер
@@ -10,6 +10,6 @@ module.exports = {
 !imgur + количество - рандомные картинки с имгура (!NSFW!)
 !lightshot + количество -  рандомные картинки с лайтшота
 !etf2l + ссылка на стим - инфа по последнему сезону етф2л для данного стим профиля
-!leagues + ссылка на стим - ссылки на ETF2L, UGC, RGL для данного стим профиля\``);
+!leagues + ссылка на стим - ссылки на ETF2L, UGC, RGL для данного стим профиля\`\`\``);
 	},
 };
